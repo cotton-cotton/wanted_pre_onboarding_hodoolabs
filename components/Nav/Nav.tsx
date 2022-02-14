40711cbfc5dd687016db30156058243ab56efe69
SangHoon <joa3668@gmail.com>
2022-02-14 22:39:42 +0900

    Init: Initial setting

---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,7 @@
+import * as S from './Nav.style';
+
+const Nav = () => {
+  return <S.Container>Nav</S.Container>;
+};
+
+export default Nav;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Nav from '../components/Nav/Nav';
+import { GlobalStyle } from '../styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Hoodolabs</title>
+      </Head>
+      <GlobalStyle />
+      <Nav />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
