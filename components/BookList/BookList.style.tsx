8671366de0baa1520801c89a69b9399d6a2d3291
niboo <moonevening8@gmail.com>
2022-02-16 01:24:35 +0900

    Add: 슬라이더 구현

---
+++ components/BookList/BookList.style.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 868px;
+`;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 1150px;
+  margin: 0 auto;
+`;
+export const TitleContainer = styled.div`
+  margin-top: 100px;
+`;
+export const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 600;
+  line-height: 50px;
+  text-align: center;
+  white-space: pre-wrap;
+`;
+export const SubTitle = styled.h4`
+  margin-top: 30px;
+  font-size: 20px;
+  line-height: 30px;
+  text-align: center;
+  white-space: pre-wrap;
+`;
+export const Slider = styled.div`
+  border: 1px solid black;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Banner from '../components/Banner/Banner';
 import LiveBookClass from '../components/LiveBookClass/LiveBookClass';
 import ReadingClass from '../components/ReadingClass/RedingClass';
 import SelectBooks from '../components/SelectBooks/SelectBooks';
+import BookList from '../components/BookList/BookList';
 
 const Home: NextPage = () => {
   return (
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
       <LiveBookClass />
       <SelectBooks />
       <ReadingClass />
+      <BookList />
     </>
   );
 };
